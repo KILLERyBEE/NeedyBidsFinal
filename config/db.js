d0420a62f58f4d318196f7c027543b4d53ef5071
Avdhut Madhavi <71624737+KILLERyBEE@users.noreply.github.com>
2025-09-05 19:56:48 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 const db = require("mongoose")
 require("dotenv").config();
 
-db.connect(process.env.MONGODB_URI).then(()=>
+db.connect(process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then(()=>
 {
   console.log("Connected to db");
 })
